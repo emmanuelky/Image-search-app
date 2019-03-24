40de7f0a36a1db885852c95d01e5d92571b8fc23
Emmanuel Akhere Iyere <iyereemmanuelakhere@gmail.com>
2019-03-24 12:51:55 +0100

    created searchResults component.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,49 +1,55 @@
-import React from 'react'
-import SearchResults from './SearchResults'
-import axios from 'axios';
+import React, { Component } from "react";
+import SearchResults from "./SearchResults";
+import axios from "axios";
 
-const API_KEY = `${process.env.REACT_APP_IMAGE_SEARCH}`
+const API_KEY = `${process.env.REACT_APP_IMAGE_SEARCH}`;
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      searchResult: []
+    };
+  }
 
- class SearchBar extends React.Component {
-     state = {
-         value: "",
-         searchResult:[]
-     }
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
 
-     handleChange = (e) =>{
-         this.setState({
-             value: e.target.value
-         })
-     }
-     handleSubmit = (e) => {
-         e.preventDefault()
-         const searchValue = this.state.value
-         axios.get(`https://api.unsplash.com/search/photos/?client_id=${API_KEY}&per_page=15&orientation=landscape&query=${searchValue}}`).then(res => {
-             this.setState({
-                 
-                 searchResult: res.data.results
-             })     
- })
-     }
-     
+  handleSubmit = e => {
+    e.preventDefault();
+    const searchValue = this.state.value;
+    axios
+      .get(
+        `https://api.unsplash.com/search/photos/?client_id=${API_KEY}&per_page=15&orientation=landscape&query=${searchValue}}`
+      )
+      .then(res => {
+        this.setState({
+          searchResult: res.data.results
+        });
+      });
+  };
 
   render() {
     return (
       <div>
-          <form onSubmit={this.handleSubmit} className="search-container">
-                  <input
-                        class="field-container"
-                        type='text'
-                        value={this.state.value}
-                        placeholder="Search Images..."
-                        onChange={this.handleChange}/>
-             
-          <button className="btn-warning" type="submit"><i class="fa fa-search"></i></button>
-          </form>
-          <SearchResults results={this.state.searchResult}/>
+        <form onSubmit={this.handleSubmit} className="search-container">
+          <input
+            class="field-container"
+            type="text"
+            value={this.state.value}
+            placeholder="Search Images..."
+            onChange={this.handleChange}
+          />
+
+          <button className="btn-warning" type="submit">
+            <i class="fa fa-search" />
+          </button>
+        </form>
+        <SearchResults results={this.state.searchResult} />
       </div>
-    )
+    );
   }
 }
-
-export default SearchBar
---
+++ src/components/SearchResults.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class SearchResults extends Component {
+  render() {
+    return (
+      <div className="image-results">
+          {this.props.results.map(image => (
+              <div>
+                  <a href = {image.user.links.html} alt="linktounsplash" target="_blank"> 
+                  <img className="img-list" src={image.urls.small} alt="searchresult"
+                       key={image.id}/>
+                       <div class="middle">
+                          <p class="text">{image.user.username}</p>
+                        
+                      </div>
+                      </a>
+  
+
+              </div>
+
+          ))}
+      </div>
+    )
+  }
+}
